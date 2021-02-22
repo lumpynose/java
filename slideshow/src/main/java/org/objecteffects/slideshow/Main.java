fc5f448e3e14cd5812c3348181147e28ae8a001f
Rusty Wright <rusty.wright@gmail.com>
2021-02-22 15:59:37 -0800

    Initial project version

---
+++ slideshow/src/main/java/org/objecteffects/slideshow/Main.java
@@ -0,0 +1,107 @@
+package org.objecteffects.slideshow;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/*
+ * HelloWorldSwing.java requires no other files.
+ */
+
+public class Main {
+    @SuppressWarnings("unused")
+    private static int pause = 5;
+    private static String dir = "/home/rusty/pics";
+
+    public static void main(final String[] args) throws ParseException {
+        getOptions(args);
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Slideshow.createAndShowGUI();
+            }
+        });
+    }
+
+    private static void getOptions(final String[] args) throws ParseException {
+        final var options = new Options();
+        options.addOption("pause", true, "pause period for each picture");
+        options.addOption("dir", true, "directory of pictures");
+
+        final CommandLineParser parser = new DefaultParser();
+        final var cmd = parser.parse(options, args);
+
+        if (cmd.hasOption("dir")) {
+            Main.dir = cmd.getOptionValue("dir");
+        }
+
+        if (cmd.hasOption("pause")) {
+            Main.pause = Integer.parseInt(cmd.getOptionValue("pause"));
+        }
+    }
+
+    static List<Path> getFiles() {
+        final List<Path> result;
+
+        try (var walk = Files.walk(Paths.get(dir))) {
+            result = walk.filter(p -> isImgFile(p))
+                    .collect(Collectors.toList());
+        } catch (final IOException e) {
+            e.printStackTrace();
+
+            return Collections.emptyList();
+        }
+
+        System.out.printf("%d\n", Integer.valueOf(result.size()));
+
+        return result;
+    }
+
+    private static boolean isImgFile(final Path path) {
+        if (path.getFileName().toString().endsWith(".jpg")) {
+            return true;
+        }
+
+        if (path.getFileName().toString().endsWith(".JPG")) {
+            return true;
+        }
+
+        if (path.getFileName().toString().endsWith(".gif")) {
+            return true;
+        }
+
+        if (path.getFileName().toString().endsWith(".GIF")) {
+            return true;
+        }
+
+        if (path.getFileName().toString().endsWith(".jpe")) {
+            return true;
+        }
+
+        if (path.getFileName().toString().endsWith(".JPE")) {
+            return true;
+        }
+
+        if (path.getFileName().toString().endsWith(".jpeg")) {
+            return true;
+        }
+
+        if (path.getFileName().toString().endsWith(".png")) {
+            return true;
+        }
+
+        // System.out.printf("%s\n", path.getFileName().toString());
+
+        return false;
+    }
+}
---
+++ slideshow/src/main/java/org/objecteffects/slideshow/Slideshow.java
@@ -0,0 +1,239 @@
+package org.objecteffects.slideshow;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Slideshow extends JPanel implements ActionListener,
+        WindowListener,
+        ChangeListener {
+    private static final long serialVersionUID = 1L;
+    final JLabel label;
+    int delay = 1000;
+    Timer timer;
+    int numPics = 0;
+    int picNum = 0;
+    List<Path> paths;
+    final Dimension windowSize;
+
+    static void createAndShowGUI() {
+        // Create and set up the window.
+        final var frame = new JFrame("Slideshow");
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        final var windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        windowSize.setSize(windowSize.getWidth(),
+                windowSize.getHeight());
+
+        System.out.printf("width=%d, height=%d\n",
+                Integer.valueOf((int) windowSize.getWidth()),
+                Integer.valueOf((int) windowSize.getHeight()));
+
+        frame.setPreferredSize(windowSize);
+        frame.setMinimumSize(windowSize);
+
+        final var sspanel = new Slideshow(windowSize);
+
+        sspanel.setBackground(Color.BLACK);
+
+        final var pane = frame.getContentPane();
+
+        pane.setBackground(Color.BLACK);
+
+        pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+
+        pane.add(Box.createVerticalGlue());
+        pane.add(Box.createHorizontalGlue());
+
+        pane.add(sspanel);
+        pane.add(Box.createHorizontalGlue());
+
+        // Display the window.
+        frame.pack();
+        frame.setVisible(true);
+
+        sspanel.startSlideshow();
+    }
+
+    public Slideshow(final Dimension _windowSize) {
+        // Add the ubiquitous "Hello World" label.
+        this.label = new JLabel("label");
+
+        add(this.label);
+
+        this.windowSize = _windowSize;
+
+        this.timer = new Timer(this.delay, this);
+
+        this.timer.setCoalesce(true);
+    }
+
+    public void startSlideshow() {
+        this.paths = Main.getFiles();
+
+        Collections.shuffle(this.paths);
+
+        this.numPics = this.paths.size();
+
+        System.out.printf("%d\n", Integer.valueOf(this.numPics));
+
+        this.label.setText(StringUtils.EMPTY);
+
+        this.picNum = 0;
+
+        this.timer.start();
+    }
+
+    void updatePicture() {
+        final var path = this.paths.get(this.picNum);
+
+        System.out.printf("%s\n", path.toFile().toString());
+        final var icon = new ImageIcon(path.toFile().toString());
+
+        if (icon.getImageLoadStatus() != MediaTracker.COMPLETE) {
+            System.out.printf("can't load image: %s\n",
+                    path.toFile().toString());
+        } else {
+            final var scaledDimension = getScaledDimension(
+                    new Dimension(icon.getIconWidth(),
+                            icon.getIconHeight()),
+                    this.windowSize);
+
+            final var image = icon.getImage();
+            final var newimg = image.getScaledInstance(
+                    (int) scaledDimension.getWidth(),
+                    (int) scaledDimension.getHeight(),
+                    java.awt.Image.SCALE_SMOOTH);
+
+            this.label.setIcon(new ImageIcon(newimg));
+        }
+
+        this.picNum++;
+
+        if (this.picNum >= this.paths.size()) {
+            this.paths = Main.getFiles();
+
+            this.picNum = 0;
+        }
+    }
+
+    /** Add a listener for window events. */
+    void addWindowListener(final Window w) {
+        w.addWindowListener(this);
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        updatePicture();
+
+        this.timer.restart();
+    }
+
+    @Override
+    public void windowOpened(final WindowEvent e) {
+        System.out.printf("window opened\n");
+    }
+
+    @Override
+    public void windowClosing(final WindowEvent e) {
+        System.out.printf("window closing\n");
+    }
+
+    @Override
+    public void windowClosed(final WindowEvent e) {
+        System.out.printf("window closed\n");
+    }
+
+    @Override
+    public void windowIconified(final WindowEvent e) {
+        System.out.printf("window iconified\n");
+    }
+
+    @Override
+    public void windowDeiconified(final WindowEvent e) {
+        System.out.printf("window deiconified\n");
+    }
+
+    @Override
+    public void windowActivated(final WindowEvent e) {
+        System.out.printf("window activated\n");
+    }
+
+    @Override
+    public void windowDeactivated(final WindowEvent e) {
+        System.out.printf("window deactivated\n");
+    }
+
+    @Override
+    public void stateChanged(final ChangeEvent e) {
+        System.out.printf("state changed\n");
+    }
+
+    @SuppressWarnings("unused")
+    private static Dimension getOffset(final Dimension imgSize,
+            final Dimension screenSize) {
+        var hDiff = 0;
+        var vDiff = 0;
+
+        if (screenSize.getWidth() > imgSize.getWidth()) {
+            hDiff = (int) (screenSize.getWidth() - imgSize.getWidth());
+        }
+
+        if (screenSize.getHeight() > imgSize.getHeight()) {
+            vDiff = (int) (screenSize.getHeight() - imgSize.getHeight());
+        }
+
+        final var offset = new Dimension(hDiff / 2, vDiff / 2);
+
+        return offset;
+    }
+
+    private static Dimension getScaledDimension(final Dimension imgSize,
+            final Dimension boundary) {
+        final var scaledDimension = new Dimension(imgSize);
+
+        // first check if we need to scale width
+        if (imgSize.width > boundary.width) {
+            // scale width to fit
+            scaledDimension.width = boundary.width;
+            // scale height to maintain aspect ratio
+            scaledDimension.height = scaledDimension.width * imgSize.height
+                    / imgSize.width;
+        }
+
+        // then check if we need to scale even with the new height
+        if (scaledDimension.height > boundary.height) {
+            // scale height to fit instead
+            scaledDimension.height = boundary.height;
+            // scale width to maintain aspect ratio
+            scaledDimension.width = scaledDimension.height * imgSize.width
+                    / imgSize.height;
+        }
+
+        return scaledDimension;
+    }
+}
